07c89a1233459f631587476dde480c5dae5faaf5
root <root@1a09decc5662>
2017-07-31 13:32:32 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "Dublin1_.2",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "DublinUI",
+  "homePage" : "Main",
+  "name" : "DublinUI",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "15.0"
+};
